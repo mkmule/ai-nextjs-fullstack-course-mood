c81c8605260dc54337e351b34b5816c16c926566
Serhiy <serhiy.mos@gmail.com>
2024-01-11 12:46:54 +0400

    Create user flow

--- src/app/new-user/page.tsx
+++ src/app/new-user/page.tsx
@@ -1,4 +1,27 @@
-const NewUser = () => {
+import { prisma } from '@/utils/db';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const createNewUser = async () => {
+  const user: any = await currentUser();
+  const match = await prisma.user.findUnique({ where: { clerkId: user.id as string } });
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
+  redirect('/journal');
+};
+
+const NewUser = async () => {
+
+  await createNewUser();
+
   return (
     <div>Hi user</div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Link from 'next/link';
+import { auth } from '@clerk/nextjs';
+
 
 export default function Home() {
+
+  const { userId } = auth();
+  let href = userId ? '/journal' : '/new-user';
+
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
@@ -8,7 +14,7 @@ export default function Home() {
         <p className="text-2xl text-white/60 mb-4">Tracking your mood on a daily basis, be honest, really</p>
 
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">get started</button>
           </Link>
         </div>
