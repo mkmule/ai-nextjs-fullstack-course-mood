91e573db1638de0b4d1f6a82e3f5e300b51f453e
Serhiy <serhiy.mos@gmail.com>
2024-01-11 12:56:23 +0400

    Added user profile widget

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import { UserButton } from '@clerk/nextjs';
+
 const DashboardLayout = ({ children }: any) => {
   return (
     <div className="h-screen w-screen relative">
@@ -6,7 +8,9 @@ const DashboardLayout = ({ children }: any) => {
       </aside>
       <div className="ml-[200px]">
         <header className="h-[60px] border-b border-black/10">
-          Hello
+          <div className="h-full w-full px-6 flex items-center justify-end">
+            <UserButton />
+          </div>
         </header>
         <div>
           {children}
