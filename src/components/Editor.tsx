25b175454d02e7399f6e39af611d5f1aaad8a3d2
Serhiy <serhiy.mos@gmail.com>
2024-01-11 17:49:11 +0400

    Style side bar

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -20,7 +20,7 @@ const Editor = ({ entry }: any) => {
   return (
     <div className="w-full h-full">
       {isLoading && (<div>...loading</div>)}
-      <textarea className="h-full w-full p-8 text-xl outline" value={value} onChange={e => setValue(e.target.value)} />
+      <textarea className="h-full w-full p-8 text-xl" value={value} onChange={e => setValue(e.target.value)} />
     </div>
   );
 };
