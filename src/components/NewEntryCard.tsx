e9d3faa492809e688debd5e5861907ffbdb4498b
Serhiy <serhiy.mos@gmail.com>
2024-01-11 13:30:55 +0400

    Styled new entry

--- src/components/NewEntryCard.tsx
+++ src/components/NewEntryCard.tsx
@@ -1,6 +1,25 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 const NewEntryCard = () => {
+  const router = useRouter();
+
+  const handleOnClick = async () => {
+    // const { data } = await newEntry();
+    const data = { id: 1 };
+    router.push(`/journal/${data.id}`);
+  };
+
   return (
-    <div>New entry</div>
+    <div
+      className="cursor-pointer overflow-hidden rounded-lg bg-white shadow"
+      onClick={handleOnClick}
+    >
+      <div className="px-4 py-5 sm:p-6">
+        <span className="text-3xl">New Entry</span>
+      </div>
+    </div>
   );
 };
 
