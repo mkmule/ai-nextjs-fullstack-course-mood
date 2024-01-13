92a4265143a746fcc185fae519a33f1581baa49c
Serhiy <serhiy.mos@gmail.com>
2024-01-13 17:05:17 +0400

    Updated analysis on update, use upsert (Create or update if exists)

--- src/app/api/journal/[id]/route.ts
+++ src/app/api/journal/[id]/route.ts
@@ -1,6 +1,7 @@
 import { getUserByClerkId } from '@/utils/auth';
 import { prisma } from '@/utils/db';
 import { NextResponse } from 'next/server';
+import { analyse } from '@/utils/ai';
 
 export const PATCH = async (request: Request, { params }: any) => {
   const user = await getUserByClerkId();
@@ -15,6 +16,22 @@ export const PATCH = async (request: Request, { params }: any) => {
     },
     data: {
       content,
+    },
+  });
+
+  const analysis = await analyse(updatedEntry.content);
+  await prisma.analysis.upsert({
+    where: {
+      entryId: updatedEntry.id,
+    },
+    // @ts-ignore
+    create: {
+      entryId: updatedEntry.id,
+      ...analysis,
+    },
+    // @ts-ignore
+    update: {
+      ...analysis,
     },
   });
 
